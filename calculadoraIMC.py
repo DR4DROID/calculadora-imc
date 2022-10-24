9c7cdcfccded593e580f27883b760a0f1c9cb690
DR4DROID <gnvieira@gmail.com>
2022-10-24 17:21:37 -0300

    Adicionado codigo
    Adicionado ao codigo leitura de dados

---
+++ calculadoraIMC.py
@@ -0,0 +1,11 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+peso = float(input("Digite seu peso: "))
+
+print("-" * 30)
+print("OS dados coletados foram:")
+print("Seu nome: ",nome)
+print("Sua idade: ",idade," anos")
+print("Sua altura: ",altura, " mts")
+print("Seu peso ", peso," kgs")
