dd0feb05a78f25ce515b7aef1367bcd3ba18198c
DR4DROID <gnvieira@gmail.com>
2022-10-25 16:13:32 -0300

    adicionado commentarios

--- calculadoraIMC.py
+++ calculadoraIMC.py
@@ -1,8 +1,10 @@
+#Declarando variáveis
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 altura = float(input("Digite sua altura: "))
 peso = float(input("Digite seu peso: "))
 
+#saida de dados 
 print("-" * 30)
 print("OS dados coletados foram:")
 print("Seu nome: ",nome)
