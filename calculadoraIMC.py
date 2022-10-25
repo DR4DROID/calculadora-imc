34536b78788a468e74e26930ed3b963c8036584c
DR4DROID <gnvieira@gmail.com>
2022-10-25 16:51:26 -0300

    adicionado parte logica

--- calculadoraIMC.py
+++ calculadoraIMC.py
@@ -3,6 +3,7 @@ nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 altura = float(input("Digite sua altura: "))
 peso = float(input("Digite seu peso: "))
+imc = peso/altura**2 # fómula para calcular imc
 
 #saida de dados 
 print("-" * 30)
@@ -11,3 +12,19 @@ print("Seu nome: ",nome)
 print("Sua idade: ",idade," anos")
 print("Sua altura: ",altura, " mts")
 print("Seu peso ", peso," kgs")
+print("Seu IMC = ",imc)
+
+if imc < 16:
+    print("MAgreza Grave!")
+elif imc < 18.5:
+    print("Magreza Leve!")
+elif imc < 25:
+    print("Saudável!")
+elif imc < 30:
+    print("Sobrepeso!!!!!")
+elif imc < 35:
+    print("Obesidade grau I!!!!")
+elif imc < 40:
+    print("Obesidade grau 2 ?!")
+else :
+    print("Obesidade Grau III")
